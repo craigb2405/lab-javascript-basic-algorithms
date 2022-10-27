1ae76a90de19da241675d3b3e202996610c75436
Craig Butler <craig.butler@jrbsolutions.co.uk>
2022-10-27 21:51:34 +0100

    completed iterations 1 and 2, currently working on iteration 3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Craig"
+let hacker2 = "Max"
+
+console.log(`the drive's name is ${hacker1}`)
+console.log(`the navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker2.length > hacker1.length) {
+    console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+} else {
+    console.log  ("Wow, you both have equally long names, XX characters!")
+}
 
 // Iteration 3: Loops
+// Iteration 3.1
+
+let driverName = ""
+for (let i = 0; i < hacker1.length; i++) {
+    return hacker1.charAt(i).toUpperCase();
+    
+}
+
+
+
